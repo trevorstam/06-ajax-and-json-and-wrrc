5f618229c9fc5039d0bec10bfbd01d13b6db33b2
Daniel Frey <dann.frey@me.com>
2018-08-21 10:41:52 -0700

    added comments / readme

---
+++ trevor-daniel/README.md
@@ -0,0 +1,26 @@
+## Documentation
+
+```md
+# Project Name - 06 - Ajax and JSON
+
+**Author**: Trevor and Daniel
+**Version**: 1.0.0 (increment the patch/fix version number up if you make more commits past your first submission)
+
+## Overview
+Dynamically grabbing code from local storage to create an article view page. If the local storage doesn't exit, it will be created for you.
+
+## Getting Started
+Download our code, and run it in a server environment.
+
+## Architecture
+Ajax, JSON, jQuery, random bits of JavaScript, and HTML. The template is created with Handlebars.
+
+## Change Log
+<!-- Use this are to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an examples:
+
+01-01-2001 4:59pm - Application now has a fully-functional express server, with GET and POST routes for the book resource.
+
+## Credits and Collaborations
+<!-- Give credit (and a link) to other people or resources that helped you build this application. -->
+-->
+```
--- trevor-daniel/scripts/article.js
+++ trevor-daniel/scripts/article.js
@@ -12,10 +12,8 @@ function Article (rawDataObj) {
 // REVIEW: Instead of a global `articles = []` array, let's attach this list of all articles directly to the constructor function. Note: it is NOT on the prototype. In JavaScript, functions are themselves objects, which means we can add properties/values to them at any time. In this case, the array relates to ALL of the Article objects, so it does not belong on the prototype, as that would only be relevant to a single instantiated Article.
 Article.all = [];
 
-// let rawData;
-
 // COMMENT: Why isn't this method written as an arrow function?
-// PUT YOUR RESPONSE HERE
+// This is not an arrow function becuase "this" requires specific context.
 Article.prototype.toHtml = function() {
   let template = Handlebars.compile($('#article-template').text());
 
@@ -23,7 +21,7 @@ Article.prototype.toHtml = function() {
 
   // COMMENT: What is going on in the line below? What do the question mark and colon represent? How have we seen this same logic represented previously?
   // Not sure? Check the docs!
-  // PUT YOUR RESPONSE HERE
+  // this is referring to the published date, and is assigning a date based on when it was published. If the article has not been published, it will show "draft".
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
   this.body = marked(this.body);
 
@@ -35,7 +33,8 @@ Article.prototype.toHtml = function() {
 // REVIEW: This function will take the rawData, how ever it is provided, and use it to instantiate all the articles. This code is moved from elsewhere, and encapsulated in a simply-named function for clarity.
 
 // COMMENT: Where is this function called? What does 'rawData' represent now? How is this different from previous labs?
-// PUT YOUR RESPONSE HERE
+// Article.loadAll is called during the fetchAll function. Either when there is data in the local storage, or after passing the data into local storage.
+// in the previous lab, the data was called from the blogArticles.js, whereas now we are calling it from local storage.
 Article.loadAll = articleData => {
   articleData.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)))
 
@@ -46,23 +45,20 @@ Article.loadAll = articleData => {
 Article.fetchAll = () => {
   // REVIEW: What is this 'if' statement checking for? Where was the rawData set to local storage?
   if (localStorage.rawData) {
-
-    Article.loadAll(JSON.parse(localStorage.rawData));//added data as param. Didn't work
+    // this will parse the local storage
+    Article.loadAll(JSON.parse(localStorage.rawData));
+    // now that we have data in local storage, we need to init the page again.
     articleView.initIndexPage();
-
-
   } else {
     //get the json from liveserver url, gets into the data folder and grabs the hackerIpsum.json
     $.getJSON('http://127.0.0.1:5500/data/hackerIpsum.json')
       .then(
         data => {
+          // this is setting the data from the json into local storage.
           localStorage.setItem('rawData', JSON.stringify(data));
           Article.loadAll(data);
-
           articleView.initIndexPage();
-
-        })
+        });
   }
-
-}
+};
 
